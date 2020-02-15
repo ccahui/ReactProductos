384df081b84c8ac79e61b3090e3becea0baa1fcc
ccahui <kccahui@unsa.edu.pe>
2020-02-15 15:49:21 -0500

    Filtro de Busqueda y Filtro por Stock

--- src/index.js
+++ src/index.js
@@ -2,44 +2,102 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react'
 
 class FilterTableProduct extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            filterStock: false,
+        };
+    }
+    onChangeFilterStock = () => {
+        this.setState({
+            filterStock: !this.state.filterStock
+        });
+    }
+
+    onChangeFilterText = (texto) => {
+        this.setState({
+            filterText: texto
+        });
+    }
+    
     render() {
         return (
             <div>
                 <h1>Productos</h1>
-                <SearchBar />
-                <ProductTable productos = {this.props.productos}/>
+                <SearchBar filterStock = {this.state.filterStock}  onChangeFilterStock={this.onChangeFilterStock} onChangeFilterText={this.onChangeFilterText}/>
+                <ProductTable productos = {this.props.productos} filterStock={this.state.filterStock} filterText = {this.state.filterText}/>
             </div>
         )
     }
 }
 
 class SearchBar extends Component {
+
+    onChange = (e) =>{
+        this.props.onChangeFilterText(e.target.value);
+    }
+
     render() {
         return (
             <div>
-                <input name="search"/> <br/>
-                <input type="checkbox"/> Productos en stock
+                <input name="search" onChange={this.onChange}/> <br/>
+                <input type="checkbox" onChange={this.props.onChangeFilterStock} /> Productos en stock
             </div>
         )
     }
 }
 
 class ProductTable extends Component {
+
+    filterText(product, filterText) {
+        const productName = product.name.toLowerCase();
+        const text = filterText.toLowerCase();
+
+        return productName.indexOf(text) === -1
+    }
+
+    createCategoryRow(category) {
+       const props = {
+           category,
+           key: category,
+       };
+       const component =  React.createElement(ProductCategoryRow, props);
+
+       return component;
+    }
+
+    createProductRow(product) {
+        const props = {
+            product,
+            key: product.name
+        };
+        const component = React.createElement(ProductRow, props);
+        
+        return component;
+    }
+
     render() {
         const rowsTable = [];
         let lastCategory = null;
+
+        const filterText = this.props.filterText;
+        const filterStock = this.props.filterStock;
+
         this.props.productos.forEach(product => {
-           
+            if (this.filterText(product, filterText)) {
+                return;
+              }
+
+            if (filterStock && !product.stocked) {
+                return;
+              }
+
             if(product.category !== lastCategory) {
-                rowsTable.push(
-                    React.createElement(ProductCategoryRow, {
-                      category: product.category,
-                      key: product.category }));
+                rowsTable.push(this.createCategoryRow(product.category));
             }
-            rowsTable.push(
-                React.createElement(ProductRow, {
-                  product: product,
-                  key: product.name }));
+
+            rowsTable.push(this.createProductRow(product));
             
             lastCategory = product.category;
         });
@@ -71,13 +129,20 @@ class ProductCategoryRow extends Component {
 }
 
 class ProductRow extends Component {
+    
+    stockAgotado(name) {
+        return <span style={{color:'red', fontWeight: 'bold'}}>{name}</span>;
+    }
+    
     render() {
+        const product = this.props.product;
+        const name = product.stocked ? product.name : this.stockAgotado(product.name);
         return (
             <tr>
-                <td> {this.props.product.price} </td>
-                <td> {this.props.product.name} </td>
+                <td> {product.price} </td>
+                <td> {name} </td>
             </tr>
-        )
+        );
     }
 }
 
